56cde84f9fbb6c6e98caab25fe7f126015fc5c95
Théophile Gindre <theophile.gindre@gmail.com>
2021-05-01 17:40:41 +0200

    changed the api

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const Client = require('./lib/Client')
 
 /**
+ * @typedef {Object} ClientConfig
+ * @property {string} [serverUrl] The server location
+ * @property {Function} [onError] Callback fired when the server sends an error
+ */
+
+/**
  * Create and return a Client instance
  * @param {ClientConfig} config config object for the client
  * @returns {Client} an instance of Server
  */
-function createClient(config)
+export function createClient(config)
 {
     /**
      * Create the Client instance
      * @type {Client}
      */
     const client = new Client(config)
-
-    // Bind the events
-    client.bindEvents()
 
     return client
 }
